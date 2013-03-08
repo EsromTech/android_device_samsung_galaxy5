ebd2215ad08e78913557cade95f077d031e47a78
Esrom <esromgalaxy5@gmail.com>
2013-03-08 20:19:48 -0300

    update sources gingerbread

---
+++ BoardConfig.mk
@@ -0,0 +1,132 @@
+#
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := false
+
+# inherit from the proprietary version
+-include vendor/samsung/galaxy5/BoardConfigVendor.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/galaxy5/include
+
+# Camera
+BOARD_USE_FROYO_LIBCAMERA := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/galaxy5/prebuilt/kernel
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/galaxy5/prebuilt/recovery_kernel
+BOARD_NAND_PAGE_SIZE := 4096 -s 128
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_PAGE_SIZE := 0x00001000
+
+# Graphics
+BOARD_EGL_CFG := device/samsung/galaxy5/prebuilt/lib/egl/egl.cfg
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_USE_SCREENCAP := true
+#BOARD_NO_RGBX_8888 := true
+
+# cat /proc/LinuStoreIII/bmlinfo
+# FSR VERSION: FSR_1.2.1p1_b139_RTM
+# minor       position           size     units       id
+#    1: 0x00000000-0x00180000 0x00180000      6        1
+#    2: 0x00180000-0x00200000 0x00080000      2        2
+#    3: 0x00200000-0x002c0000 0x000c0000      3        3
+#    4: 0x002c0000-0x01bc0000 0x01900000    100        4
+#    5: 0x01bc0000-0x03400000 0x01840000     97       23
+#    6: 0x03400000-0x03900000 0x00500000     20       25
+#    7: 0x03900000-0x05200000 0x01900000    100        5
+#    8: 0x05200000-0x05400000 0x00200000      8        6 
+#    9: 0x05400000-0x05e00000 0x00a00000     40        7
+#   10: 0x05e00000-0x06800000 0x00a00000     40        8
+#   11: 0x06800000-0x068c0000 0x000c0000      3        9
+#   12: 0x068c0000-0x11a40000 0x0b180000    710       21
+#   13: 0x11a40000-0x1dc00000 0x0c1c0000    775       22
+#   14: 0x1dc00000-0x1f500000 0x01900000    100       24
+
+# Recovery
+BOARD_BML_BOOT := /dev/block/bml9
+BOARD_BML_RECOVERY := /dev/block/bml10
+TARGET_BOOTLOADER_BOARD_NAME := galaxy5
+TARGET_OTA_ASSERT_DEVICE := galaxy5,GT-I5500
+BOARD_RECOVERY_HANDLES_MOUNT := true
+BOARD_HAS_DOWNLOAD_MODE := true
+BOARD_LDPI_RECOVERY := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 186122240
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 203161600
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_RECOVERY_INITRC := device/samsung/galaxy5/recovery/recovery.rc
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxy5/recovery/graphics.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/galaxy5/recovery/recovery_ui.c
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Audio
+TARGET_PROVIDES_LIBAUDIO := true
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := galaxy5
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# USB
+BOARD_CUSTOM_USB_CONTROLLER := ../../device/samsung/galaxy5/UsbController.cpp
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+
+# WiFi
+BOARD_WLAN_ATHEROS_SDK      := true
+BOARD_WEXT_NO_COMBO_SCAN    := true
+PRODUCT_WIRELESS_TOOLS      := true
+WPA_SUPPLICANT_VERSION      := VER_0_5_X
+BOARD_WPA_SUPPLICANT_DRIVER := AWEXT
+BOARD_WLAN_DEVICE           := ar6000
+WIFI_DRIVER_MODULE_PATH     := "/system/wifi/ar6000.ko"
+WIFI_DRIVER_MODULE_ARG      := ""
+WIFI_DRIVER_MODULE_NAME     := ar6000
+
+# 3G
+BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
+
+# JIT / Optimizations
+#WITH_DEXPREOPT := true
+JS_ENGINE := v8
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := bcm2049
+#BOARD_USE_BROADCOM_FM_VOLUME_HACK := true
---
+++ device_galaxy5.mk
@@ -0,0 +1,208 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/galaxy5/overlay
+
+PRODUCT_PACKAGES += \
+    copybit.msm7k \
+    g5sensord \
+    gps.galaxy5 \
+    gralloc.galaxy5 \
+    libOmxCore \
+    lights.msm7k \
+    rzscontrol \
+    screencap \
+    sensors.galaxy5 \
+    setup_fs
+
+# Recovery tools
+PRODUCT_PACKAGES += \
+    dump_image \
+    e2fsck \
+    erase_image \
+    flash_image \
+    make_ext4fs
+
+# EuropaParts
+PRODUCT_PACKAGES += \
+    EuropaParts
+
+# Service Mode Secret Code
+PRODUCT_PACKAGES += \
+    SamsungServiceMode
+
+# Live wallpaper packages
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    librs_jni \
+    LiveWallpapers \
+    VisualizationWallpapers
+
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES += \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+# EuropaParts support files
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/bin/handle_europaparts:system/bin/handle_europaparts \
+    device/samsung/galaxy5/prebuilt/etc/init.europaparts.rc:system/etc/init.europaparts.rc \
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/europaparts/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/europaparts/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+
+# Hardware properties
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Kernel modules
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/lib/modules/ar6000.ko:system/wifi/ar6000.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/cifs.ko:system/lib/modules/cifs.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/fsr.ko:root/lib/modules/fsr.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/fsr_stl.ko:root/lib/modules/fsr_stl.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/rfs_fat.ko:system/lib/modules/rfs_fat.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/rfs_glue.ko:system/lib/modules/rfs_glue.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/ramzswap.ko:system/lib/modules/ramzswap.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/sec_param.ko:root/lib/modules/sec_param.ko \
+    device/samsung/galaxy5/prebuilt/lib/modules/tun.ko:system/lib/modules/tun.ko
+
+# Keymaps
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/usr/keylayout/europa_keypad0.kl:system/usr/keylayout/europa_keypad0.kl \
+    device/samsung/galaxy5/prebuilt/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+
+# Board-specific init & scripts
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
+    device/samsung/galaxy5/prebuilt/init.gt-i5500.rc:root/init.gt-i5500.rc \
+    device/samsung/galaxy5/prebuilt/ueventd.gt-i5500.rc:root/ueventd.gt-i5500.rc \
+    device/samsung/galaxy5/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf
+
+# Workaround for wifi & bluetooth mac addresses
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/bin/get_macaddrs:system/bin/get_macaddrs
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/etc/bluetooth/audio.conf:system/etc/bluetooth/audio.conf
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/etc/dhcpd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/samsung/galaxy5/prebuilt/etc/wifi/hostapd.conf:system/etc/wifi/hostapd.conf \
+    device/samsung/galaxy5/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# VOLD configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/etc/vold.fstab:system/etc/vold.fstab
+
+# Media profiles
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/etc/media_profiles.xml:/system/etc/media_profiles.xml
+
+# Audio
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxy5/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/samsung/galaxy5/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt
+
+# Ensure that /cache is never used for deodexing
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1
+
+# Enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
+
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+
+# RIL properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libargs=-d/dev/smd0 \
+    rild.libpath=/system/lib/libsec-ril.so \
+    ro.telephony.ril_class=samsung \
+    ro.telephony.sends_barcount=1
+
+# Keymap properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    android.keychar.europa_keypad0=/system/usr/keychars/europa_keypad0.kcm.bin \
+    android.keylayout.europa_headset=/system/usr/keylayout/europa_headset.kl \
+    android.keylayout.europa_keypad0=/system/usr/keylayout/europa_keypad0.kl \
+    hw.keyboards.0.devname=europa_keypad0 \
+    hw.keyboards.65538.devname=europa_headset
+
+# Networking properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=45
+
+# Performance & graphics properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapsize=24m \
+    persist.sys.purgeable_assets=1 \
+    persist.sys.scrollingcache=2 \
+    persist.sys.use_dithering=1 \
+    ro.media.dec.jpeg.memcap=20000000 \
+    ro.opengles.version=131072 \
+    ro.sf.lcd_density=120
+
+# Compcache properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.compcache.default=18
+
+# Audio attenuation
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.fm-attn=0 \
+    persist.sys.headset-attn=0 \
+    persist.sys.speaker-attn=0
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/samsung/galaxy5/prebuilt/kernel
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product-if-exists, vendor/samsung/galaxy5/galaxy5-vendor.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_ww_supl.mk)
+
+# LDPI assets
+PRODUCT_LOCALES += ldpi mdpi
+
+PRODUCT_NAME := samsung_galaxy5
+PRODUCT_DEVICE := galaxy5
+PRODUCT_MODEL := Galaxy5
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
